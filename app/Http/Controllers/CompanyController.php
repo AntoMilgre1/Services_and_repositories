297f31948e1d530cf16f70985225a046c3abaca2
Mac Mini Office 1 <binumathew1988@gmail.com>
2023-07-28 12:48:27 +0530

    Updated

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -7,36 +7,62 @@ use App\Services\CompanyService;
 
 class CompanyController extends Controller
 {
- 
-   
-    //
-    public static function create(Request $request){
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $companies = CompanyService::index();
+        return response()->json(['status'=>true,
+                                 'message'=>'All Company List',
+                                 'data'=>$companies],200);   
+    }
 
-       
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
         $company = CompanyService::create($request->all());
         return response()->json(['status'=>true,
                                  'message'=>'New Company Created',
                                  'data'=>$company],200);   
-
     }
-    public static function read($id){
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
         $individualCompany=CompanyService::read($id);
         return response()->json(['status'=>true,
                                  'message'=>'Company Data',
                                  'data'=>$individualCompany],200);
     }
-    public static function update(Request $request){
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
         $company = CompanyService::update($request->all());
         return response()->json(['status'=>true,
                                  'message'=>'Company Data Updated',
                                  'data'=>$company],200);
     }
-    public static function delete($id){
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
         CompanyService::delete($id);
         return response()->json(['status'=>true,
                                  'message'=>'Company Data Deleted'
                              ],204);
     }
+   
+  
 }
 
 
